b6e420d55ccab5845de785ba1b29e608674783b5
Daan <daanhenkek@gmail.com>
2017-07-19 19:15:10 +0200

    -Testing GitHub webhook in Discord

--- Client/js/BrowserScript.js
+++ Client/js/BrowserScript.js
@@ -3,5 +3,6 @@ var patat = 23111212;
 window.onload = function () {
     gameInstance = new Game(document.getElementById("canvas-game"), ExecutionModes.MODE_BROWSER);
     gameInstance.test();
+    //a
 };
 //# sourceMappingURL=BrowserScript.js.map
